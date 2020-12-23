6c40b9e6d0bfe13bb461c12e0b683e66c4ef8f71
yogipro <70940478+yogipro@users.noreply.github.com>
2020-12-23 18:19:52 +0330

    Update homePageComponent.js

--- homePageComponent.js
+++ homePageComponent.js
@@ -3,7 +3,7 @@
 
 class HomePageComponent extends React.Component{
 	render(){
-		return (<div><h1>Welcome Back</h1>
+		return (<div id = "wrapper_div"><h1>Welcome Back</h1>
     	<span class = "index-button" id = "cv_button" onClick= {openCV}>My CV</span>
     	<span class = "index-button" id = "about_me_button">About Me</span>
     	<span class = "index-button" id = "article_button" onClick = {showArticle}>Article</span></div>)
