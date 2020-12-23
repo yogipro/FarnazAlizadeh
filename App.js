21e7e71edbfd525ac50c0639294a39f8adc06eb1
yogipro <70940478+yogipro@users.noreply.github.com>
2020-12-23 20:33:32 +0330

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,27 @@
 
 
 
+
+
+
+
+
 function showArticle(){
 	ReactDOM.render(<ArticleComponent/>,document.getElementById("root"))
+	var root = document.getElementById('root');
+	root.style.top = 0;
+	root.style.textAlign = "left"
+	root.backgroundColor = "red";
+	root.style.height = "auto"
+	
 }
 function displayMainMenu(){
-	ReactDOM.render(<HomePageComponent/>,document.getElementById("root"))
+	ReactDOM.render(<HomePageComponent/>,document.getElementById("root"));
+	var root = document.getElementById('root');
+	root.style.top = "20%";
+	root.style.textAlign = "center";
+	root.style.height = "300px"
+	
 }
 function openCV(){
 	window.location = "cv.html"
